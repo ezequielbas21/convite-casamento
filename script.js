985f528471e4240539bd21e029c00907e1a45f7e
ezequielbas21 <128620835+ezequielbas21@users.noreply.github.com>
2026-07-15 12:41:23 +0000

    Corrige animacao de abertura da carta

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 const seloBotao = document.querySelector("#selo-botao");
 const selo = document.querySelector("#selo");
 const abaEnvelope = document.querySelector("#aba-envelope");
-const envelopeConjunto = document.querySelector("#envelope-conjunto");
 const carta = document.querySelector("#carta");
 const texto = document.querySelector("#texto");
 
 let conviteAberto = false;
 
 seloBotao.addEventListener("click", function () {
+
   if (conviteAberto) {
     return;
   }
 
   conviteAberto = true;
 
-  /* 1. O texto desaparece */
+  /* 1. Texto desaparece */
   texto.classList.add("sumindo");
 
-  /* 2. O selo diminui e desaparece */
+  /* 2. Selo desaparece */
   selo.classList.add("abrindo");
 
-  /* 3. A aba recebe a classe de abertura */
+  /* 3. Aba abre */
   setTimeout(function () {
     abaEnvelope.classList.add("aberta");
   }, 450);
 
+  /* 4. Carta começa a sair */
+  setTimeout(function () {
+    carta.classList.add("saindo");
+  }, 950);
 
-  /* 5. A carta sobe e passa para frente */
-setTimeout(function () {
-  carta.classList.add("saindo");
-}, 950);
+  /* 5. Carta termina de sair */
+  setTimeout(function () {
+    carta.classList.remove("saindo");
+    carta.classList.add("aberta");
+  }, 1500);
 
-setTimeout(function () {
-  carta.classList.remove("saindo");
-  carta.classList.add("aberta");
-}, 1450);
+});
